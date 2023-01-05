2eb1fa6462ecca7b87fb30f8b06eaa7cbe9782f5
mayracs <mayra14renata@gmail.com>
2023-01-05 15:44:12 -0300

    teste básico com operadores booleanos

--- tipos-variaveis/src/OperadoresBoolean.java
+++ tipos-variaveis/src/OperadoresBoolean.java
@@ -1,7 +1,8 @@
 public class OperadoresBoolean {
     public static void main(String[] args) {
         boolean variavel = true;
-
+        //System.out.println(variavel);
+        
         variavel = !variavel;
 
         System.out.println(variavel);
