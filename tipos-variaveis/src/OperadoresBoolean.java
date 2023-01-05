3e88e6c29abb8c37a2172d86c7c45d94ec3208fc
mayracs <mayra14renata@gmail.com>
2023-01-05 15:42:43 -0300

    teste operador unário de decremento de valor

---
+++ tipos-variaveis/src/OperadoresBoolean.java
@@ -0,0 +1,9 @@
+public class OperadoresBoolean {
+    public static void main(String[] args) {
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+}
