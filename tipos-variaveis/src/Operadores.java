45c5fbff8f52ad06c4fa10db0b880a6122f2f36c
mayracs <mayra14renata@gmail.com>
2023-01-05 15:32:30 -0300

    teste de operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,20 +1,14 @@
 public class Operadores {
     public static void main(String[] args) {
-        String concatenacao ="?"; 
+       int numero = 5;
 
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
+       numero = -numero;
 
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);
+       System.out.println(numero);
 
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
+       numero = numero * -1;
+       
+       System.out.println(numero);
 
-        concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
     }
 }
